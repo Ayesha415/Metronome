36c3638c8d682a3a183b9a271e3a533f436a7caf
ayeshasadiqa <ayeshasadiqa415@gmail.com>
2020-12-14 13:24:18 +0500

    files added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Metronome from './components/metronome'
+function App() {
+  return (
+    <div className="App">
+      <Metronome />
+    </div>
+  );
+}
+
+export default App;
